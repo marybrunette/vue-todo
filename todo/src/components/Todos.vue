221e4c0e611c5b986a113ff9055adb96d48fd057
michaelmagon <michael_magon@outlook.com>
2020-10-19 13:37:47 +0800

    todo item and todo list

---
+++ todo/src/components/Todos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="t in todos" v-bind:key="t.id">
+      <TodoItem v-bind:todo="t"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import TodoItem from './TodoItem.vue'
+
+export default {
+  name: "Todos",
+  props: ["todos"],
+  components: {
+    TodoItem
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
